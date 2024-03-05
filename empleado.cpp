5f85245a21702fe5869593437b97ceb72e106ff5
cdionicio <carlitosd1988@gmail.com>
2024-03-04 22:42:15 -0600

    agregar classe empleado

---
+++ empleado.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ using namespace std;
+ class Empleado {
+ 	
+ 	private : string nombre, apellido, direccion,telefono, Fecha_de_Nacimento;
+ 	
+ 	void crear();
+ 	void leer ();
+ 	void actualizar();
+ 	void borrar ();
+ 			
+ };
